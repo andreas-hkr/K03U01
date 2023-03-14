2f3b9d41f6e9001414a3fe3f56db2b0b0b57c280
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-14 13:27:18 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Läs in en sfärs radie och beräkna
+        //   - Volymen = (4 * pi * r*r*r) / 3
+        //   - Arean   = 4 * pi * r*r
+
+
+    }
+}
